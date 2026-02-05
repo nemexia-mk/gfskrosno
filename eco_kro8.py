c22d895cc9de5374a09b1abca43eb9121a279230
nemexia-mk <swierkmateusz97@gmail.com>
2026-02-05 14:34:49 +0100

    Update and rename eco_kro7.py to eco_kro8.py

--- eco_kro8.py
+++ eco_kro8.py
@@ -15,7 +15,7 @@ API_KEY = "9ec822a2-8fd2-44dc-902c-2d574bd8850f"
 FTP_HOST_CFG = os.getenv("FTP_HOST", "twoj_host_ftp.pl") 
 FTP_USER_CFG = os.getenv("FTP_USER", "uzytkownik")
 FTP_PASS_CFG = os.getenv("FTP_PASS", "haslo")
-FTP_DIR = "/"  # Ścieżka na serwerze (np. "/public_html/pogoda/" lub "/" dla głównego)
+FTP_DIR = "stacja.meteo-krosno.pl/"  # Ścieżka na serwerze (np. "/public_html/pogoda/" lub "/" dla głównego)
 
 STATIONS = {
     "Turaszowka": "E8:DB:84:99:BF:2B", # eco_krosno.csv
@@ -275,3 +275,4 @@ if __name__ == "__main__":
         upload_ftp(csv_file, REMOTE_FILES[station_name])
 
     print("\nKoniec – ostatnia pełna doba pobrana i wysłana.")
+
